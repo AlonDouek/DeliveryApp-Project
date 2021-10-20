35038d8fc7c67998b1d3a4d1c26796826e540993
User <User@LAB3-13>
2021-10-20 13:39:41 +0300

    ADDED SOME STAFF AT SIGNUP PAGE AND SIGNUPVIEWMODEL NAD SMALL CHANGES IN LOG IN PAGE BURHHHHHHHHHHHHH -_-

--- DeliveryApp/DeliveryApp/ViewModels/SignUpPageViewModel.cs
+++ DeliveryApp/DeliveryApp/ViewModels/SignUpPageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace DeliveryApp.ViewModels
@@ -15,5 +16,89 @@ namespace DeliveryApp.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
+
+
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+
+        //still not 100% sure if i do this might remove later think about it
+
+        //Maybe slpilt
+
+        //private PaymentInfo paFo;
+        //public PaymentInfo PaFo
+        //{
+        //    get { return paFo; }
+        //    set
+        //    {
+        //        paFo = value;
+        //        OnPropertyChanged("PaFo");
+        //    }
+        //}
+
+        public ICommand SubmitCommand { protected set; get; }
+
+        public SignUpPageViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
     }
 }
