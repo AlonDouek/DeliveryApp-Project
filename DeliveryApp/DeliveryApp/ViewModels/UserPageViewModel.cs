4d6911535310b56465c71ef04bb5fc262638e10e
User <User@LAB3-13>
2021-10-31 08:59:27 +0200

    USER PAGE + MORE MAYBE

--- DeliveryApp/DeliveryApp/ViewModels/UserPageViewModel.cs
+++ DeliveryApp/DeliveryApp/ViewModels/UserPageViewModel.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DeliveryApp.Models;
+using DeliveryApp.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace DeliveryApp.ViewModels
@@ -27,14 +30,78 @@ namespace DeliveryApp.ViewModels
             }
         }
 
-        private string username;
-        public string Username
+        private string password;
+
+        public string Password
         {
-            get { return username; }
+            get { return password; }
+
             set
             {
-                username = value;
-                OnPropertyChanged("Username");
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        public ICommand ChangeCredentialCommand { protected set; get; }
+
+        public UserPageViewModel()
+        {
+            ChangeCredentialCommand = new Command(MoveChangeCredential);
+        }
+        public async void MoveChangeCredential()
+        {
+            await App.Current.MainPage.Navigation.PushModalAsync(new Views.ChangeCredentialsPage());
+            DeliveryAPIProxy proxy = DeliveryAPIProxy.CreateProxy();
+            User user = await proxy.LoginAsync(Email, Password);
+            if (user == null)
+            {
+                await App.Current.MainPage.DisplayAlert("error", "failed, please Log In", "ok");
+                await App.Current.MainPage.Navigation.PushModalAsync(new Views.LogInPage());
+
+            }
+            else
+            {
+                
+                Page p = new NavigationPage(new Views.ChangeCredentialsPage());
+                App.Current.MainPage = p;
+
+                //d
             }
         }
     }
