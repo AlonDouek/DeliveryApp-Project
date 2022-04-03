c8c0ccd9ac2dd5f081a46cf0a580c121af024c4c
User <User@LAB3-13>
2022-04-03 12:01:04 +0300

    JHGJG

--- DeliveryApp/DeliveryApp/ViewModels/UserPageViewModel.cs
+++ DeliveryApp/DeliveryApp/ViewModels/UserPageViewModel.cs
@@ -60,8 +60,8 @@ namespace DeliveryApp.ViewModels
         {
             int bp = 0;
             User = ((App)App.Current).CurrentUser;
-            //CreditCard = User.CreditCard;
-            CreditCard = "0000000000000000";
+            CreditCard = User.CreditCard;
+            //CreditCard = "0000000000000000";
             ChangeCredentialCommand = new Command(MoveChangeCredential);
             LogoutCommand = new Command(Logout);
         }
